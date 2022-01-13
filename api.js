e3c3f50c5d52480dc79e10a14e4629b1c0bc01f9
Marcello Lopes <marcello35seco@gmail.com>
2022-01-13 14:53:58 -0300

    Delete api.js

--- api.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = '720147a2279f9efc7ebd32c994413cfc';
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const language = 'language=pt-BR';
